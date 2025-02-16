fb290336c8979ac53ef4855603c05c4e5d51173d
Андрей <perminand@gmail.com>
2025-02-16 08:10:02 +0300

    feat: create evreka

--- evreka-server/src/main/java/ru/perminov/game/Main.java
+++ evreka-server/src/main/java/ru/perminov/game/Main.java
@@ -2,13 +2,12 @@ package ru.perminov.game;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class Game {
+@EnableEurekaServer
+public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(Game.class, args);
+        SpringApplication.run(Main.class, args);
     }
 }
